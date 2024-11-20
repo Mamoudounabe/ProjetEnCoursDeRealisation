71652eb6306cede415776b45739d328877bf3349
Kieran Aydin <Kieran_Aydin@etu.u-bourgogne.fr>
2024-11-20 15:06:16 +0100

    component filiere

---
+++ datasup/src/app/filieres/filieres-popularite/filieres-popularite.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-filieres-popularite',
+  standalone: true,
+  imports: [],
+  templateUrl: './filieres-popularite.component.html',
+  styleUrl: './filieres-popularite.component.css'
+})
+export class FilieresPopulariteComponent {
+
+}
