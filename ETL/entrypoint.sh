3a947dce0d34aaa0ed9c65f33866b2ed14c9f3c2
Lucas Ber <lucas_ber@etu.u-bourgogne.fr>
2024-11-20 11:10:57 +0100

    Ajout du fichier .sh pour docker

---
+++ ETL/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Attendre que Neo4j soit prêt
+echo "Attente du démarrage de Neo4j..."
+until nc -z -v -w30 neo4j 7687; do
+  echo "En attente de Neo4j..."
+  sleep 10
+done
+
+echo "Neo4j est prêt, démarrage de l'ETL..."
+python main.py
