0841d9c7728d0ef466e3bf5cfa27b64ce389dca4
guesbeogo <guesbeogo75@gmail.com>
2024-11-26 22:11:42 +0100

    contenu du composant orientation

--- datasup/src/app/orientation/orientation-page/orientation-page.component.ts
+++ datasup/src/app/orientation/orientation-page/orientation-page.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-orientation-page',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './orientation-page.component.html',
-  styleUrl: './orientation-page.component.css'
+  styleUrls: ['./orientation-page.component.css']
 })
 export class OrientationPageComponent {
+  showSpecialities: boolean = false;
 
+  toggleSpecialities() {
+    this.showSpecialities = !this.showSpecialities;
+  }
 }
