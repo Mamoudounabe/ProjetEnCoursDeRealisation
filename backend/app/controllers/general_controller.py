0babc25b0e44ef4228c8413b56e8c013bc81082d
Inès <inesecharoux@gmail.com>
2024-12-13 13:20:04 +0100

    ajout d'endpoints généraux

--- backend/app/controllers/general_controller.py
+++ backend/app/controllers/general_controller.py
@@ -8,6 +8,27 @@ router = APIRouter()
 @router.get("/nodes/{node_type}", response_model=List[dict], tags=["General"])
 def get_all_nodes_by_type(node_type: str):
     """
-    Endpoint pour récupérer tous les noeuds d'un type spécifique.
+    Récupère tous les nœuds d'un type spécifique.
     """
     return GeneralManager.get_all_nodes_by_type(node_type)
+
+@router.get("/general/annee/{annee}", response_model=List[dict], tags=["General"])
+def get_all_filiere_etablissement_admission_proportion_by_annee(annee: str):
+    """
+    Récupère toutes les admissions et proportions par filière et établissement pour une année donnée.
+    """
+    return GeneralManager.get_all_filiere_etablissement_admission_proportion_by_annee(annee)
+
+@router.get("/general/filiere/{filiere_id}", response_model=dict, tags=["General"])
+def get_filiere_by_id(filiere_id: int):
+    """
+    Récupère une filière spécifique selon son identifiant.
+    """
+    return GeneralManager.get_filiere_by_id(filiere_id)
+
+@router.get("/general/etablissements/{annee}", response_model=List[dict], tags=["General"])
+def get_filieres_etablissements_by_year(annee: str):
+    """
+    Récupère les filières et établissements pour une année donnée.
+    """
+    return GeneralManager.get_filieres_etablissements_by_year(annee)
--- backend/app/managers/general_manager.py
+++ backend/app/managers/general_manager.py
@@ -18,18 +18,68 @@ class GeneralManager:
             results = session.run(query)
             return [record["n"] for record in results]
 
+    @staticmethod
+    def get_all_filiere_etablissement_admission_proportion_by_annee(annee: str) -> List[dict]:
+        """
+        Retourne toutes les admissions et les proportions dans toutes les filieres reliée à un établissement par année
+        """
+        query = f"""
+        MATCH (f:Filiere)<-[:OFFERS]-(e:Etablissement)-[:HAS_ADMISSION]->(a:Admission),
+              (e)<-[:HAS_ETABLISSEMENT]-(s:Session),
+              (a)-[:HAS_PROPORTION]->(p:Proportion)
+        WHERE s.annee = "2021"
+        RETURN f AS filiere, 
+               e AS etablissement, 
+               a AS admission,
+               p AS proportion;
+        """
+        db = Neo4JDriver.get_driver()
+        with db.session() as session:
+            results = session.run(query, annee=annee)
+            return [
+                {
+                    "filiere": record["f"],
+                    "etablissement": record["e"],
+                    "admission": record["a"],
+                    "proportion": record["p"]
+                }
+                for record in results
+            ]
 
-# Liste des noeuds supporté
-SUPPORTED_NODE_TYPES = [
-    "Admission",
-    "Bachelier",
-    "Candidat",
-    "Classement",
-    "ClassementRang",
-    "ComplementPhase",
-    "Etablissement",
-    "Filiere",
-    "Proportion",
-    "Session",
-    "Terminale"
-]
+    @staticmethod
+    def get_filieres_etablissements_by_year(annee: str) -> List[dict]:
+        """
+        Retourne les filières et établissements pour une année donnée.
+        """
+        query = """
+        MATCH (f:Filiere)<-[:OFFERS]-(e:Etablissement),
+              (e)<-[:HAS_ETABLISSEMENT]-(s:Session)
+        WHERE s.annee = $annee
+        RETURN f AS filiere, e AS etablissement
+        ORDER BY etablissement DESC
+        """
+        db = Neo4JDriver.get_driver()
+        with db.session() as session:
+            results = session.run(query, annee=annee)
+            return [
+                {
+                    "filiere": record["f"],
+                    "etablissement": record["e"]
+                }
+                for record in results
+            ]
+
+    @staticmethod
+    def get_filiere_by_id(filiere_id: int) -> dict:
+        """
+        Retourne une filière spécifique selon son identifiant.
+        """
+        query = """
+            MATCH (f:Filiere)
+            WHERE id(f) = $filiere_id
+            RETURN f
+            """
+        db = Neo4JDriver.get_driver()
+        with db.session() as session:
+            result = session.run(query, filiere_id=filiere_id).single()
+            return {"filiere": result["f"]} if result else None
