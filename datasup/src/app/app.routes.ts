dd6a1a22ffc769fdc6987872573af93c0abfc477
mn420660 <mamoudou_nabe@etu.u-bourgogne.fr>
2025-02-10 03:03:52 +0100

    mis à jour de main

--- datasup/src/app/app.routes.ts
+++ datasup/src/app/app.routes.ts
@@ -8,11 +8,13 @@ import { StatistiquesPageComponent } from './statistiques/statistiques-page/stat
 import { AccueilPageComponent } from './accueil-page/accueil-page.component';
 import { FormationsPageComponent } from './formations/formations-page/formations-page.component';
 import { FormationDetailComponent } from './formations/formation-detail/formation-detail.component';
+import{AproposComponent} from './apropos/apropos.component';
 export const routes: Routes = [
     { path: '', component: AccueilPageComponent },   
     { path: 'filieres', component: FilieresPageComponent },
     { path: 'rapports-analyses', component: RapportsPageComponent },
     { path: 'statistiques', component: StatistiquesPageComponent },
     {path: 'formations', component: FormationsPageComponent},
-    { path: 'formations/:id', component: FormationDetailComponent }
+    { path: 'formations/:id', component: FormationDetailComponent },
+    {path: 'apropos', component: AproposComponent}
 ];
