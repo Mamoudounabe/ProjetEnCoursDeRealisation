2f56ad52cbf128b650d7035f803dcb3eb5a7d874
mn420660 <mamoudou_nabe@etu.u-bourgogne.fr>
2025-02-21 11:18:54 +0100

    m a j recuperation des données formation de la base de données

--- datasup/src/app/formations/formations-page/formations-page.component.ts
+++ datasup/src/app/formations/formations-page/formations-page.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,OnDestroy  ,ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ApiService } from '../../core/services/api.service';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-formations-page',
   standalone: true,
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    MatProgressSpinnerModule
   ],
   templateUrl: './formations-page.component.html',
   styleUrls: ['./formations-page.component.css']
@@ -24,21 +26,38 @@ export class FormationComponent implements OnInit {
   searchQuery: string = '';
   page: number = 1;
   pageSize: number = 10;
-  totalPages: number = 1;
 
-  constructor(private apiService: ApiService) {}
+  totalPages: number = 50000;
 
-  ngOnInit() {
+
+  isLoading: boolean = true;
+  private timeoutId: any; // Identifier pour le setTimeout
+
+
+  constructor(private apiService: ApiService , private cdr: ChangeDetectorRef) {}
+
+ ngOnInit() {
     this.fetchFormations();
-  }
+  } 
 
+
+
+  
   fetchFormations() {
+    this.isLoading = true;  // Affiche le spinner en début de chargement
+  
     this.apiService.getFiliereEtablissements(this.page, this.pageSize).subscribe({
       next: (response) => {
         console.log("Données reçues :", response);
+        this.formations = response.items;
+        this.filteredFormations = this.formations;
+        console.log("Formations :", this.formations);
+        
+        this.isLoading = false;  // Arrête le spinner après que les données soient reçues
       },
       error: (error) => {
         console.error("Erreur API :", error);
+        this.isLoading = false;  // Arrête aussi le spinner en cas d'erreur
       }
     });
   }
@@ -46,6 +65,7 @@ export class FormationComponent implements OnInit {
   
 
   filterFormations() {
+    
     const query = this.searchQuery.toLowerCase();
     this.filteredFormations = this.formations.filter(formation => 
       formation.filiere_formation?.toLowerCase().includes(query) ||
