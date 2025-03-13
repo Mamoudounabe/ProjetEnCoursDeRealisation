baf9fd9155001772ab9c01491e3418190733e7b1
mn420660 <mamoudou_nabe@etu.u-bourgogne.fr>
2025-03-13 14:16:31 +0100

    maj

--- datasup/src/app/app.routes.ts
+++ datasup/src/app/app.routes.ts
@@ -9,7 +9,7 @@ import { AccueilPageComponent } from './accueil-page/accueil-page.component';
 import { FormationsPageComponent } from './formations/formations-page/formations-page.component';
 import { FormationDetailComponent } from './formations/formation-detail/formation-detail.component';
 import { FilieresDetailsComponent } from './filieres/filieres-details/filieres-details.component';
-import { ComparatifEtablissementsComponent } from './filieres/filieres-region/comparatif-etablissements';
+import { ComparatifEtablissementsComponent } from './filieres/filieres-region/comparatif-etablissements.component';
 
 import{AproposComponent} from './apropos/apropos.component';
 export const routes: Routes = [
@@ -22,4 +22,4 @@ export const routes: Routes = [
     {path: 'apropos', component: AproposComponent},
     {path:'filieres/:id',component:FilieresDetailsComponent},
     {path:'etablissements',component:ComparatifEtablissementsComponent}
-];
+]
