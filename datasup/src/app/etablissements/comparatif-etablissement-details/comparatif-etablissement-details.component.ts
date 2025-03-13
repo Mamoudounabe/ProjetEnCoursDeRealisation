dffc503e70f6756982d83fb1d2c70598f8ef6063
nabe root <mamoudou-nabe@etu.u-bourgogne.fr>
2025-03-14 00:35:26 +0100

    m a j

---
+++ datasup/src/app/etablissements/comparatif-etablissement-details/comparatif-etablissement-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparatif-etablissement-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './comparatif-etablissement-details.component.html',
+  styleUrl: './comparatif-etablissement-details.component.css'
+})
+export class ComparatifEtablissementDetailsComponent {
+
+}
---
+++ datasup/src/app/etablissements/comparatif-etablissement-page/comparatif-etablissement-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparatif-etablissement-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './comparatif-etablissement-page.component.html',
+  styleUrl: './comparatif-etablissement-page.component.css'
+})
+export class ComparatifEtablissementPageComponent {
+
+}
