bc5cb1e97b78bdd4f0b6d726b65f226af52592fc
nabe root <mamoudou-nabe@etu.u-bourgogne.fr>
2025-03-14 02:21:17 +0100

    maj

---
+++ datasup/src/app/geographiques/geo-details/geo-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-geo-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './geo-details.component.html',
+  styleUrl: './geo-details.component.css'
+})
+export class GeoDetailsComponent {
+
+}
