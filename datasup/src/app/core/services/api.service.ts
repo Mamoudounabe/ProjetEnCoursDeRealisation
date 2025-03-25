d77a015f0fe41c929c52cc04056397612879d169
mn420660 <mamoudou_nabe@etu.u-bourgogne.fr>
2025-03-25 22:42:12 +0100

    amj

--- datasup/src/app/core/services/api.service.ts
+++ datasup/src/app/core/services/api.service.ts
@@ -2,6 +2,9 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams  } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { config } from '../../../environments/config';
+import { throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
@@ -14,28 +17,33 @@ export class ApiService {
 
 // Récupérer les établissements avec pagination
  // Récupérer les établissements avec pagination
- getFiliereEtablissements(page: number = 1, pageSize: number = 8): Observable<any> {
+/*  getFiliereEtablissements(page: number = 1, pageSize: number = 8): Observable<any> {
   let params = new HttpParams()
     .set('page', page.toString())
     .set('page_size', pageSize.toString());
 
-  // Log de l'URL pour vérifier si c'est correct
   console.log(`Request URL: ${this.apiUrl}/filiere/etablissement/admission?page=${page}&page_size=${pageSize}`);
 
   return this.http.get<any>(`${this.apiUrl}/filiere/etablissement/admission`, { params });
-}
+} */
 
-  // Méthode pour obtenir des données depuis l'API
-  getData(endpoint: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}/${endpoint}`);
+
+
+  getFiliereEtablissements(query: string = '', page: number = 1, pageSize: number = 8): Observable<any> {
+    let params = new HttpParams()
+      .set('query', query || '') 
+      .set('page', page.toString())
+      .set('page_size', pageSize.toString());
+  
+    // Log de l'URL pour vérifier si c'est correct
+    console.log(`Request URL: ${this.apiUrl}/filiere/etablissement/admission?query=${query}&page=${page}&page_size=${pageSize}`);
+  
+    return this.http.get<any>(`${this.apiUrl}/filiere/etablissement/admission`, { 
+      params, 
+      headers: { 'Content-Type': 'application/json; charset=utf-8' } 
+    });
   }
-
-  // Méthode pour envoyer des données à l'API
-  postData(endpoint: string, data: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/${endpoint}`, data);
-  }
-
-
+  
 
 
 
@@ -65,12 +73,30 @@ export class ApiService {
   
 
 
-/*   getFilieresByDetails(filiereID: number, anneeactuelle: string): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/filiere/Candidat/Bachelier/${filiereID}`, {
-      params: { anneeactuelle: anneeactuelle },
-  }
-   */
 
+
+// Fonction pour récupérer les établissements pour la comparaison
+
+
+
+
+getEtablissementsByComp(etablissementIDs: number[], anneeactuelle: string): Observable<any[]> {
+  if (!etablissementIDs || etablissementIDs.length < 2) {
+    throw new Error('Vous devez sélectionner au moins deux établissements pour la comparaison.');
+  }
+
+  // Créer les paramètres de requête avec plusieurs occurrences de 'etablissementIDs'
+  let params = new HttpParams()
+    .set('anneeactuelle', anneeactuelle);
+
+  etablissementIDs.forEach(id => {
+    params = params.append('etablissementIDs', id.toString());
+  });
+
+  console.log('Requête envoyée avec les paramètres:', params.toString());
+
+  // Exécuter la requête HTTP GET
+  return this.http.get<any[]>(`${this.apiUrl}/etablissements/comparaison`, { params });
 }
 
 
@@ -78,6 +104,4 @@ export class ApiService {
 
 
 
-
-
-
+}
--- datasup/src/app/filieres/filieres-page/filieres-page.component.ts
+++ datasup/src/app/filieres/filieres-page/filieres-page.component.ts
@@ -15,18 +15,17 @@ import {MatProgressBarModule} from '@angular/material/progress-bar';
 
 
 @Component({
-  selector: 'app-filieres-page',
-  standalone: true,
-  imports: [ FormsModule,
-    FormsModule,
-    MatPaginatorModule,
-    MatSliderModule,
-    MatProgressSpinnerModule,
-    CommonModule,
-    MatProgressBarModule
-  ],
-  templateUrl: './filieres-page.component.html',
-  styleUrl: './filieres-page.component.css'
+    selector: 'app-filieres-page',
+    imports: [FormsModule,
+        FormsModule,
+        MatPaginatorModule,
+        MatSliderModule,
+        MatProgressSpinnerModule,
+        CommonModule,
+        MatProgressBarModule
+    ],
+    templateUrl: './filieres-page.component.html',
+    styleUrl: './filieres-page.component.css'
 })
 export class FilieresPageComponent {
 
@@ -56,7 +55,7 @@ ngOnInit(){
   this.chargementFilieres();
 }
 
-
+/* 
 
 chargementFilieres(){
   this.isloading = true;
@@ -89,7 +88,41 @@ chargementFilieres(){
 
   );
 }
+ */
 
+
+
+
+chargementFilieres(query: string = '') {
+  this.isloading = true;  // Affiche le spinner en début de chargement
+
+  // Assurez-vous que `query` est une chaîne
+  if (typeof query !== 'string') {
+    query = '';
+  }
+
+  // Envoyer les paramètres en tant qu'entiers, sans conversion en chaîne
+  this.apiService.getFiliereEtablissements(query, this.page, this.pageSize).subscribe({
+    next: (response) => {
+      console.log("Données reçues :", response);
+      this.formations = response.items || [];
+      this.filtrerFilieres = [...this.formations]; // Initialisation correcte
+      this.totalItems = response.total_items; // Mise à jour du nombre total d'éléments
+      this.totalPages = Math.ceil(this.totalItems / this.pageSize); // Mise à jour du nombre total de pages
+      console.log("Formations :", this.formations);
+      this.isloading = false;  // Arrête le spinner après que les données soient reçues
+    },
+    error: (error) => {
+      console.error("Erreur API :", error);
+      if (error.status === 200 && error.error instanceof SyntaxError) {
+        console.error("La réponse de l'API n'est pas au format JSON attendu.");
+      } else {
+        console.error("Erreur lors de la récupération des données :", error.message);
+      }
+      this.isloading = false;  // Arrête aussi le spinner en cas d'erreur
+    }
+  });
+}
 
 
     filtrerFormations() {
