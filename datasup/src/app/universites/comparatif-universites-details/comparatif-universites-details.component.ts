1fda573fcd74a6c33c09038b1e643eac0488dbb2
mn420660 <mamoudou_nabe@etu.u-bourgogne.fr>
2025-03-29 01:56:12 +0100

    maj

---
+++ datasup/src/app/universites/comparatif-universites-details/comparatif-universites-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comparatif-universites-details',
+  imports: [],
+  templateUrl: './comparatif-universites-details.component.html',
+  styleUrl: './comparatif-universites-details.component.css'
+})
+export class ComparatifUniversitesDetailsComponent {
+
+}
