7e5da4ab3a47ab3f385efd340b4e837a53295c1f
Constant <Constant-Ouedraogo@etu.u-bourgogne.fr>
2025-04-13 16:51:58 +0200

    mofi

--- datasup/src/app/core/components/header/header.component.ts
+++ datasup/src/app/core/components/header/header.component.ts
@@ -1,30 +1,34 @@
 import { Component, ChangeDetectionStrategy, HostListener } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
-
-import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-header',
-  standalone: true, // important si standalone
-  imports: [
-    CommonModule,
-    RouterModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
-  ],
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+    selector: 'app-header', // Assure-toi d'utiliser Angular 15+
+    imports: [
+        RouterLink,
+        CommonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule
+    ],
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
   isMenuOpen = false;
 
+  constructor(private router: Router) {}
+
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  navigateToFormationspage(): void {
+    this.router.navigate(['/formations']);
   }
 
   @HostListener('document:click', ['$event'])
